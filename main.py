0f510749a15a8b40c4b54c0e8e6c3cbe587b5721
WilliamASC22 <109669967+WilliamASC22@users.noreply.github.com>
2022-10-19 09:15:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 print(str("Hi"))
+
+
+
+
+timer = 0
+baked_good = []
+bakery = 0
+
+while bakery == 0 :
+  answer = input ("What kind of baked good do you want to make?")
+  if (answer == "cookies" or answer == "cake" or answer == "pie"):
+    baked_good.append(answer)
+    if (answer == "cookies"):
+      timer = 12
+    if (answer == "cake"):
+      timer = 30
+    if (answer == "pie"):
+      timer = 20
+    timer = timer + 1
+    while timer > 0:
+      timer = timer - 1
+      print("You have " + str(timer) + " minutes left")
+      if timer == 0:
+        print(answer + " is out of the oven!")
+  if (answer != "cookies" and  answer != "cake" and answer != "pie"):
+    print("I don't recognize that!")
